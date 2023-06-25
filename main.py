0bc98d5daee7f5dd9406fce035efeba70aeb6c2f
Prem kumar <70461546+0premthecoder@users.noreply.github.com>
2023-06-25 05:14:57 +0000

    stage

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# MTEyMjM0MzM2NzU5MDgyNjA5NQ.G_1Q3D.CkfvI0ILb6ZAqnEL3GF3zwHEMkFtwQIbEkfczc dicord token
-# app id : 1122343367590826095
-# public key: d5c32dc7e6b72b7af47b8ead91cf55aebbfdfc9eb59615682d94cb06eee70831
-
-# JsSUhf4PQ7pr_6dwSK1F3hDGRWuHREUI secret key
 
 
 # This example requires the 'message_content' intent.
